d9612501c2946151d7066a83312b9d6bdea87356
Osvaldo Cintra Garcia <wizardos2011@gmail.com>
2026-02-28 16:46:41 -0300

    Add ClinicalNote entity and module with relationships

--- src/clinical-notes/clinical-notes.module.ts
+++ src/clinical-notes/clinical-notes.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { ClinicalNote } from './entities/clinical-note.entity';
 import { ClinicalNotesService } from './clinical-notes.service';
 import { ClinicalNotesController } from './clinical-notes.controller';
 
 @Module({
   controllers: [ClinicalNotesController],
   providers: [ClinicalNotesService],
+  imports: [TypeOrmModule.forFeature([ClinicalNote])],
+  exports: [TypeOrmModule],
 })
 export class ClinicalNotesModule {}
--- src/clinical-notes/entities/clinical-note.entity.ts
+++ src/clinical-notes/entities/clinical-note.entity.ts
@@ -1,1 +1,39 @@
-export class ClinicalNote {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+import { User } from '../../auth/entities/user.entity';
+import { ClinicalRecord } from '../../clinical-records/entities/clinical-record.entity';
+
+@Entity('clinical_notes')
+export class ClinicalNote {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToOne(() => ClinicalRecord, (record) => record.notes, {
+    onDelete: 'CASCADE',
+  })
+  @JoinColumn({ name: 'clinicalRecordId' })
+  clinicalRecord: ClinicalRecord;
+
+  @Column()
+  clinicalRecordId: string;
+
+  @ManyToOne(() => User, (user) => user.clinicalNotes)
+  @JoinColumn({ name: 'authorId' })
+  author: User;
+
+  @Column()
+  authorId: string;
+
+  @Column({ type: 'text' })
+  content: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+}
